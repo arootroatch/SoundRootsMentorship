2f719e50f06b3030ba307a954fe86c54f076985f
Alex Root-Roatch <arootroatch@gmail.com>
2024-02-13 14:59:42 -0600

    install styled componenets and babel plugin. touch babelrc and configure babel plugin

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,21 @@
+import styled from "styled-components"
 
+const Nav = styled.nav`
+  
+`
 
 export default function Navbar() {
   return (
-    <div>
-      
-    </div>
+    <Nav>
+      <div><span>Sound Roots</span><span>Mentorship</span></div>
+      <ul>
+        <li>Home</li>
+        <li>Learn</li>
+        <li>Contact</li>
+      </ul>
+      <div>
+
+      </div>
+    </Nav>
   )
 }
