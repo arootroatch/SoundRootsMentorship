0a30904467701324ef454c9670b890696aa123fe
Alex Root-Roatch <arootroatch@gmail.com>
2024-02-16 19:35:29 -0600

    edit lib/posts

--- lib/posts.tsx
+++ lib/posts.tsx
@@ -1,9 +1,9 @@
 import path from "path";
 import fs from "fs";
+import matter from "gray-matter";
 
 import { readFile, access } from "fs/promises";
 import { compileMDX } from "next-mdx-remote/rsc";
-
 
 async function getPosts() {
   const POSTS_PATH = path.join(process.cwd(), "content");
@@ -11,33 +11,18 @@ async function getPosts() {
     .readdirSync(POSTS_PATH)
     // Only include md(x) files
     .filter((path: string) => /\.mdx?$/.test(path));
-    
-  const posts = postFilePaths.map((filePath: string)=>{
+
+  const posts = postFilePaths.map((filePath: string) => {
     const source = fs.readFileSync(path.join(POSTS_PATH, filePath));
-    const { content, frontmatter } = await compileMDX<{ title: string }>({
-      source: filePath,
-      options: { parseFrontmatter: true },
-    });
-      // do something with frontmatter, like set metadata or whatever
-      return {
-        content,
-        data: frontmatter,
-        filePath
-      }
-  })
-  // const { content, frontmatter } = await compileMDX<{ title: string }>({
-  //   source: `---
-  //     title: RSC Frontmatter Example
-  //     ---
-  //     # Hello World
-  //     This is from Server Components!
-  //   `,
-  //   options: { parseFrontmatter: true },
-  // });
-  // return (
-  //   <>
-  //     <h1>{frontmatter.title}</h1>
-  //     {content}
-  //   </>
-  // );
+    const { content, data } = matter(source);
+    const dataObj = JSON.stringify(data);
+
+    return {
+      content,
+      data: JSON.parse(dataObj),
+      filePath,
+    };
+  });
+
+  return posts;
 }
