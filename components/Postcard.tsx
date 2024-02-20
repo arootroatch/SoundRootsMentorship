c1a726fcd16ed9542f8b9bcbf00dae0be72b7115
Alex Root-Roatch <arootroatch@gmail.com>
2024-02-20 01:36:13 -0600

    moved postcard styles to a css module and deleted styled components code

--- components/Postcard.tsx
+++ components/Postcard.tsx
@@ -1,7 +1,6 @@
-"use client"
 
 import Image from "next/image";
-import styled from "styled-components";
+import styles from "@/components/postcard.module.css";
 import { agencyFont } from "@/lib/fonts";
 import Link from "next/link";
 
@@ -15,32 +14,6 @@ interface PostcardProps {
   href: string;
 }
 
-const PostcardStyles = styled.div`
-  width: 200px;
-  height: 400px;
-  margin: 1rem;
-  display: inline-block;
-  font-family: "Franklin Gothic", "Arial Narrow", Arial, sans-serif;
-  letter-spacing: 0.1rem;
-  border-radius: 0.3rem;
-  overflow: hidden;
-  .image {
-    position: relative; // necessary for NextJS image fill property
-    height: 50%;
-  }
-  .info {
-    padding: 1rem;
-  }
-  h3 {
-    letter-spacing: 0.2rem;
-    margin-top: 1rem;
-    margin-bottom: 1rem;
-  }
-  .date,
-  .description {
-    font-size: 0.8rem;
-  }
-`;
 
 export default function Postcard(props: PostcardProps) {
   const date = new Date(props.date);
@@ -50,17 +23,19 @@ export default function Postcard(props: PostcardProps) {
     day: "2-digit",
   });
   return (
-    <PostcardStyles>
+    <div className={styles.postcard}>
       <Link as={props.as} href={props.href}>
-        <div className='image'>
-          <Image alt={props.image} src={props.image} fill className='image' />
+        <div className={styles.image}>
+          <Image alt={props.image} src={props.image} fill className={styles.image} />
         </div>
-        <div className='info'>
-          <p className='date'>{formattedDate}</p>
-          <h3 className={agencyFont.className}>{props.title}</h3>
-          <p className='description'>{props.description}</p>
+        <div className={styles.info}>
+          <p className={styles.date}>{formattedDate}</p>
+          <h3 className={`${agencyFont.className}`}>
+            {props.title}
+          </h3>
+          <p className={styles.description}>{props.description}</p>
         </div>
       </Link>
-    </PostcardStyles>
+    </div>
   );
 }
