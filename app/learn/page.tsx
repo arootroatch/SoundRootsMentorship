b39ae6f7931881c3ec796a6319cc5d46b2c75b08
Alex Root-Roatch <arootroatch@gmail.com>
2024-03-13 01:50:13 -0500

    moved sortPostsByCategory to separate file in lib dir. refactored to use array of categories and for each

--- app/learn/page.tsx
+++ app/learn/page.tsx
@@ -5,7 +5,7 @@ import PostScroller from "@/components/PostScroller";
 import "normalize.css";
 import Hero from "@/components/Hero";
 import getPosts from "@/lib/getPosts";
-import { PostsProps } from "@/lib/interfaces";
+import sortPostsByCategory from "@/lib/sortPostsByCategory";
 
 export default async function Home() {
   const posts = await getPosts();
@@ -39,101 +39,3 @@ export default async function Home() {
   );
 }
 
-function sortPostsByCategory(posts: PostsProps[]) {
-  let categorized = [];
-  const amplifiers = posts.filter(
-    (post) => post.data.category === "Amplifiers"
-  );
-  if (amplifiers.length > 0) {
-    categorized.push(amplifiers);
-  }
-
-  const digital = posts.filter((post) => post.data.category === "Digital");
-  if (digital.length > 0) {
-    categorized.push(digital);
-  }
-
-  const effects = posts.filter((post) => post.data.category === "Effects");
-  if (effects.length > 0) {
-    categorized.push(effects);
-  }
-
-  const interconnect = posts.filter(
-    (post) => post.data.category === "Interconnect"
-  );
-  if (interconnect.length > 0) {
-    categorized.push(interconnect);
-  }
-
-  const loudspeakers = posts.filter(
-    (post) => post.data.category === "Loudspeakers"
-  );
-  if (loudspeakers.length > 0) {
-    categorized.push(loudspeakers);
-  }
-
-  const microphones = posts.filter(
-    (post) => post.data.category === "Microphones"
-  );
-  if (microphones.length > 0) {
-    categorized.push(microphones);
-  }
-
-  const mixers = posts.filter((post) => post.data.category === "Mixers");
-  if (mixers.length > 0) {
-    categorized.push(mixers);
-  }
-
-  const mixing = posts.filter((post) => post.data.category === "Mixing");
-  if (mixing.length > 0) {
-    categorized.push(mixing);
-  }
-
-  const rfSystems = posts.filter((post) => post.data.category === "RF Systems");
-  if (rfSystems.length > 0) {
-    categorized.push(rfSystems);
-  }
-
-  const science = posts.filter(
-    (post) => post.data.category === "Science of Sound"
-  );
-  if (science.length > 0) {
-    categorized.push(science);
-  }
-
-  const signalFlow = posts.filter(
-    (post) => post.data.category === "Signal Flow"
-  );
-  if (signalFlow.length > 0) {
-    categorized.push(signalFlow);
-  }
-
-  const signalProcessing = posts.filter(
-    (post) => post.data.category === "Signal Processing"
-  );
-  if (signalProcessing.length > 0) {
-    categorized.push(signalProcessing);
-  }
-
-  const softSkills = posts.filter(
-    (post) => post.data.category === "Soft Skills"
-  );
-  if (softSkills.length > 0) {
-    categorized.push(softSkills);
-  }
-
-  const soundcheck = posts.filter(
-    (post) => post.data.category === "Soundcheck"
-  );
-  if (soundcheck.length > 0) {
-    categorized.push(soundcheck);
-  }
-
-  const troubleshooting = posts.filter(
-    (post) => post.data.category === "Troubleshooting"
-  );
-  if (troubleshooting.length > 0) {
-    categorized.push(troubleshooting);
-  }
-  return categorized;
-}
