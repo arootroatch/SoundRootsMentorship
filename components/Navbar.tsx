81207befa5ba5f6ddfd202b7a7c468872a63e839
Alex Root-Roatch <arootroatch@gmail.com>
2024-03-15 02:56:31 -0500

    redesigned nested nav as separate client component with CSS animation onclick

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,17 +1,18 @@
-
-import styles from './navbar.module.css';
+import styles from "./navbar.module.css";
 import Link from "next/link";
 import { FaInstagram } from "react-icons/fa";
 import { FaSquareFacebook } from "react-icons/fa6";
 import { IoIosSearch } from "react-icons/io";
+import { IoCloseOutline } from "react-icons/io5";
+import { RxHamburgerMenu } from "react-icons/rx";
 import { mixbox, handlee, agencyFont } from "@/lib/fonts";
-
+import NestedNav from "./NestedNav";
 
 export default function Navbar() {
   return (
     <nav className={styles.nav}>
       <div className={styles.navWrapper}>
-        <div>
+        <div className={styles.leftWrapper}>
           <span className={`${styles.soundRoots} ${mixbox.className}`}>
             Sound Roots
           </span>
@@ -19,43 +20,36 @@ export default function Navbar() {
             Mentorship
           </span>
         </div>
-        <div style={{ display: "flex", alignItems: "center" }}>
-          <ul className={agencyFont.className}>
-            <li className={`${styles.nestedParent} ${styles.navLink}`}>
-              Home
-              <ul className={styles.nestedNav}>
-                <div className={styles.arrowUp}></div>
-                <li className={styles.nested}>
-                  <Link href='https://www.soundrootsproductions.com/'>
-                    Sound Roots Productions
-                  </Link>
-                </li>
-                <li className={styles.nested}>
-                  <Link href='/'>Sound Roots Mentorship</Link>
-                </li>
-              </ul>
-            </li>
-            <li className={styles.navLink}>
-              <Link href='/learn'>Learn</Link>
-            </li>
-            <li className={styles.navLink}>
-              <Link href=''>Contact</Link>
-            </li>
-          </ul>
-          <Link
-            href='https://www.facebook.com/soundrootsproductions'
-            className={styles.navIcon}
-          >
-            <FaSquareFacebook className={styles.icon} />
-          </Link>
-          <Link
-            href='https://www.instagram.com/soundrootsproductions/'
-            className='navIcon'
-          >
-            <FaInstagram className={styles.instagram} />
-          </Link>
-          <span className={styles.separator}>|</span>
-          <IoIosSearch className={styles.search} />
+        <div className={styles.rightWrapper}>
+          <div className={styles.linkWrapper}>
+            <ul className={agencyFont.className}>
+              <NestedNav />
+              <li className={styles.navLink}>
+                <Link href='/learn'>Learn</Link>
+              </li>
+              <li className={styles.navLink}>
+                <Link href=''>Contact</Link>
+              </li>
+            </ul>
+          </div>
+          <div className={styles.iconWrapper}>
+            <Link
+              href='https://www.facebook.com/soundrootsproductions'
+              className={styles.navIcon}
+            >
+              <FaSquareFacebook className={styles.icon} />
+            </Link>
+            <Link
+              href='https://www.instagram.com/soundrootsproductions/'
+              className='navIcon'
+            >
+              <FaInstagram className={styles.instagram} />
+            </Link>
+            <span className={styles.separator}>|</span>
+            <IoCloseOutline className={styles.close} />
+            <IoIosSearch className={styles.search} />
+            <RxHamburgerMenu className={styles.hamburger} />
+          </div>
         </div>
       </div>
     </nav>
