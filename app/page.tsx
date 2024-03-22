dd79b11d705e081cee54be7942e772fca68a7cfc
Alex Root-Roatch <arootroatch@gmail.com>
2024-03-21 21:01:28 -0500

    add position relative to intro wrapper to keep position absolute picture in place

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import styles from "@/app/page.module.css";
 import Intro from "@/components/home/Intro";
+import About from "@/components/home/About";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Intro />
-
+      <About />
     </main>
   );
 }
---
+++ components/home/About.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { agencyFont } from '@/lib/fonts'
+
+export default function About() {
+  return (
+    <div>
+      <h2 className={agencyFont.className}>The problem with audio education today</h2>
+    </div>
+  )
+}
