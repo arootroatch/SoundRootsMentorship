97ed6a136a12ccf5dde896f719dcb9078d676810
Alex Root-Roatch <arootroatch@gmail.com>
2024-04-05 11:24:03 -0500

    added fuse.js for fuzzy matching to searchbar

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -4,9 +4,13 @@ import { useCombobox } from "downshift";
 import { Posts } from "@/lib/interfaces";
 import styles from "./searchBar.module.css";
 import { SearchBarParams } from "@/lib/interfaces";
+import Fuse from "fuse.js";
 
 export default function SearchBar({ posts, searchOpen }: SearchBarParams) {
   const [inputItems, setInputItems] = React.useState(posts);
+  const fuse = new Fuse(posts, {
+    keys: ["data.title", "data.category"],
+  });
 
   const {
     isOpen,
@@ -19,15 +23,17 @@ export default function SearchBar({ posts, searchOpen }: SearchBarParams) {
   } = useCombobox({
     items: inputItems,
     onInputValueChange: ({ inputValue }) => {
-      setInputItems(
-        posts.filter((post) =>
-          post.data.title.toLowerCase().startsWith(inputValue.toLowerCase())
-        )
-      );
+      let search = fuse.search(inputValue);
+      let searchResults = search.map((result) => result.item);
+      setInputItems(searchResults);
     },
   });
   return (
-    <div className={`${styles.searchBar} ${searchOpen && styles.openSearch} ${isOpen && styles.openSearchAndList}`}>
+    <div
+      className={`${styles.searchBar} ${searchOpen && styles.openSearch} ${
+        isOpen && styles.openSearchAndList
+      }`}
+    >
       {/* <label {...getLabelProps()}>Choose an element:</label> */}
       <div className={styles.inputWrapper}>
         <input
@@ -43,25 +49,25 @@ export default function SearchBar({ posts, searchOpen }: SearchBarParams) {
           &#8595;
         </button> */}
       </div>
-      <ul {...getMenuProps()} className={`${styles.list} ${isOpen && styles.openList}`}>
-        {
-          inputItems.map((item, index) => (
-            <li
-              style={
-                highlightedIndex === index
-                  ? { backgroundColor: "#bde3ff3f" }
-                  : {}
-              }
-              key={`${item}${index}`}
-              {...getItemProps({ item, index })}
-              className={styles.listItem}
-            >
-              <a href={`/learn/${item.filePath.replace(/\.mdx?$/, "")}`}>
-                <div>{item.data.title}</div>
-                <div className={styles.category}>{item.data.category}</div>
-              </a>
-            </li>
-          ))}
+      <ul
+        {...getMenuProps()}
+        className={`${styles.list} ${isOpen && styles.openList}`}
+      >
+        {inputItems.map((item, index) => (
+          <li
+            style={
+              highlightedIndex === index ? { backgroundColor: "#bde3ff3f" } : {}
+            }
+            key={`${item}${index}`}
+            {...getItemProps({ item, index })}
+            className={styles.listItem}
+          >
+            <a href={`/learn/${item.filePath.replace(/\.mdx?$/, "")}`}>
+              <div>{item.data.title}</div>
+              <div className={styles.category}>{item.data.category}</div>
+            </a>
+          </li>
+        ))}
       </ul>
     </div>
   );
