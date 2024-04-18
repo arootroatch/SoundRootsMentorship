d23dabe5c395a0f500f6682eae55b4c9a4fcb305
Alex Root-Roatch <arootroatch@gmail.com>
2024-04-18 18:14:13 -0500

    add posts scroller to bottom of home page

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,13 @@ import About from "@/components/home/About";
 import Instructor from "@/components/home/Instructor";
 import Topics from "@/components/home/Topics";
 import Contact from "@/components/home/Contact";
+import PostScroller from "@/components/PostScroller";
+import getMostRecentPosts from "@/lib/getMostRecentPosts";
+import getPosts from "@/lib/getPosts";
 
 export default function Home() {
+  const posts = getPosts();
+  const recent = getMostRecentPosts(posts, 5);
   return (
     <main className={styles.main}>
       <Intro />
@@ -13,6 +18,7 @@ export default function Home() {
       <Instructor />
       <Topics />
       <Contact />
+      <PostScroller title={"Start learning now"} category={recent} />
     </main>
   );
 }
