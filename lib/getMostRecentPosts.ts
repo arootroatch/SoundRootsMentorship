ff191cc03abbd1e945573a0339cd8952b1b9b13d
Alex Root-Roatch <arootroatch@gmail.com>
2024-04-19 22:14:21 -0500

    edit getMostRecentPosts to sort based on updated date not publishing date

--- lib/getMostRecentPosts.ts
+++ lib/getMostRecentPosts.ts
@@ -4,7 +4,7 @@ export default function getMostRecentPosts(posts: Posts[], amount: number) {
   return posts
     .toSorted(
       (a, b) =>
-        new Date(b.data.date).getTime() - new Date(a.data.date).getTime()
+        new Date(b.data.updated).getTime() - new Date(a.data.updated).getTime()
     )
     .slice(0, amount);
 }
